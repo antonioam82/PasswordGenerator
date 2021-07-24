abf7048ff02f3d50f7e9c4a053e2cfbafff371be
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-07-24 21:30:40 +0200

    Update passwords_generator0.py

--- passwords_generator0.py
+++ passwords_generator0.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import string
 import sys
 import time
@@ -10,8 +11,11 @@ class app:
         self.root.geometry("899x320")
 
         self.your_password = StringVar()
+        self.length=IntVar()
 
         Entry(self.root,textvariable=self.your_password,font=('arial 20'),width=58).place(x=10,y=20)
+        Label(self.root,text="LENGTH:").place(x=10,y=100)
+        self.len=ttk.Combobox(self.root,width=10).place(x=68,y=100)
 
         self.root.mainloop()
 
