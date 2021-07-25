fef2f25eef6c01ebdb35d438c276f0264395dc13
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-07-25 16:40:49 +0200

    Update passwords_generator0.py

--- passwords_generator0.py
+++ passwords_generator0.py
@@ -16,10 +16,12 @@ class app:
         Entry(self.root,textvariable=self.your_password,font=('arial 20'),width=58).place(x=10,y=40)
         Label(self.root,text="LENGTH:").place(x=10,y=100)
         self.len=ttk.Combobox(self.root,width=10).place(x=68,y=100)
-        Label(self.root,text="MIN LOWERCASE:").place(x=240,y=100)
-        self.min_low=ttk.Combobox(self.root,width=10).place(x=343,y=100)
-        Label(self.root,text="MIN UPPERCASE:").place(x=540,y=100)
-        self.min_upp=ttk.Combobox(self.root,width=10).place(x=640,y=100)
+        Label(self.root,text="MIN LOWERCASE:").place(x=210,y=100)
+        self.min_low=ttk.Combobox(self.root,width=10).place(x=313,y=100)
+        Label(self.root,text="MIN UPPERCASE:").place(x=468,y=100)
+        self.min_upp=ttk.Combobox(self.root,width=10).place(x=570,y=100)
+        Label(self.root,text="MIN NUMBERS:").place(x=710,y=100)
+        self.min_num=ttk.Combobox(self.root,width=10).place(x=802,y=100)
 
         self.root.mainloop()
 
