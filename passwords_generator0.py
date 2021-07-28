ff96ff00e38ebc3831d119358970c79c126ece5b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-07-28 16:15:40 +0200

    Update passwords_generator0.py

--- passwords_generator0.py
+++ passwords_generator0.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from tkinter import ttk
 import string
 import random
@@ -10,6 +11,7 @@ class app:
     def __init__(self):
         self.root = Tk()
         self.root.title("CREATE YOU PASSWORD")
+        self.activated = True
         self.root.geometry("899x290")
         self.numbs = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,
                       25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,
@@ -23,7 +25,8 @@ class app:
         Label(self.root,text="LENGTH:").place(x=10,y=100)
         self.stateLabel = Label(self.root,text="",width=127)
         self.stateLabel.place(x=2,y=131)
-        Button(self.root,text="CREATE PASSWORD",width=123,height=2,command=self.init_task).place(x=12,y=158)
+        self.btnCreate = Button(self.root,text="CREATE PASSWORD",width=123,height=2,command=self.init_task)
+        self.btnCreate.place(x=12,y=158)
         Button(self.root,text="SAVE PASSWORD",width=123,height=2).place(x=12,y=208)
         self.len=ttk.Combobox(self.root,width=10,state="readonly")
         self.len.place(x=68,y=100)
@@ -50,23 +53,31 @@ class app:
     def genera_password(self):
         characts = string.ascii_letters+string.digits
         self.stateLabel.configure(text="LOOKING FOR YOUR PASSWORD...",fg="red")
-        while True:
+        while self.activated == True:
             pswrd=("").join(random.choice(characts) for i in range(int(self.len.get())))
             print(pswrd)
             if(sum(c.islower() for c in pswrd)>=int(self.min_low.get())
                 and sum(c.isupper() for c in pswrd)>=int(self.min_upp.get())
                 and sum(c.isdigit() for c in pswrd)>=int(self.min_num.get())):
-                break
+                #break
+                self.activated = False
+                
         self.stateLabel.configure(text="TASK COMPLETED.",fg="blue")
+        if self.activated == False:
+            self.btnCreate.configure(text="CREATE PASSWORD",command=self.init_task)
         self.your_password.set(pswrd)
+        self.activated = True
 
-    #def valid_entry(self,char):
-        #return char in "0123456789"
+    def cancel_process(self):
+        self.activated = False
 
     def init_task(self):
         if int(self.min_low.get()) + int(self.min_upp.get()) + int(self.min_num.get()) <= int(self.len.get()):
+            self.btnCreate.configure(text="CANCEL",command=self.cancel_process)
             t = threading.Thread(target=self.genera_password)
             t.start()
+        else:
+            messagebox.showwarning("ERROR","INVALID LENGHT")
 
 if __name__=="__main__":
     app()
