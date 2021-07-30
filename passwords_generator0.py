4d2cbf6cd70017b3748877d1ab9d781252ebf82a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-07-30 15:52:48 +0200

    Update passwords_generator0.py

--- passwords_generator0.py
+++ passwords_generator0.py
@@ -18,19 +18,20 @@ class app:
                       47,48,49,50]
         
         self.your_password = StringVar()
-        
+        #validatecommand = self.root.register(self.valid_entry)
+
         Label(self.root,text="YOUR PASSWORD").place(x=10,y=20)
         Entry(self.root,textvariable=self.your_password,font=('arial 20'),width=58).place(x=10,y=40)
         Label(self.root,text="LENGTH:").place(x=10,y=100)
         self.stateLabel = Label(self.root,text="",width=127)
         self.stateLabel.place(x=2,y=131)
-        self.btnCreate = Button(self.root,text="CREATE PASSWORD",width=123,height=2,command=self.init_task)
+        self.btnCreate = Button(self.root,text="CREATE PASSWORD",width=123,height=2,bg="gray86",command=self.init_task)
         self.btnCreate.place(x=12,y=158)
-        Button(self.root,text="SAVE PASSWORD",width=123,height=2,command=self.save_password).place(x=12,y=208)
+        Button(self.root,text="SAVE PASSWORD",width=123,height=2,bg="gray86",command=self.save_password).place(x=12,y=208)
         self.len=ttk.Combobox(self.root,width=10,state="readonly")
         self.len.place(x=68,y=100)
         Label(self.root,text="MIN LOWERCASE:").place(x=210,y=100)
-        self.min_low=ttk.Combobox(self.root,width=10,state="readonly")
+        self.min_low=ttk.Combobox(self.root,width=10,state="readonly")#validatecommand=(validatecommand,"%S"))
         self.min_low.place(x=313,y=100)
         Label(self.root,text="MIN UPPERCASE:").place(x=458,y=100)
         self.min_upp=ttk.Combobox(self.root,width=10,state="readonly")
